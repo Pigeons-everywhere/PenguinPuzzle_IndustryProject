d6f72bef4ac5258aae36389e0ac64a275e0fea57
F-oolish-Chaos <165270105+F-oolish-Chaos@users.noreply.github.com>
2026-03-18 16:52:20 +0000

    UI 1.9
    - 'GameStart' Script Change
    - Scene_00 → Scene_01 → Scene_Map → Scene_Lvl1

--- PenguinPuzzle_Y3_S2/Assets/Scripts/GameStart.cs
+++ PenguinPuzzle_Y3_S2/Assets/Scripts/GameStart.cs
@@ -3,8 +3,9 @@ using UnityEngine.SceneManagement;
 
 public class GameStart : MonoBehaviour
 {
+    [SerializeField] private string SceneToLoad;
     public void OnPlayButton()
     {
-        SceneManager.LoadScene("Map");
+        SceneManager.LoadScene(SceneToLoad);
     }
 }
